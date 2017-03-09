9db23098eec7e70ba0234e02d121ac2bf66b18d0
P14182505 <P14182505@my365.dmu.ac.uk>
2017-03-09 01:27:09 +0000

    Commit 11 [09/3/17]
    - edited the style.css -
    + added loop parametes making it 100% width of the screen.(.quoteloop)
    + added picture dimentions which are similar to the profile pic however
    the images do not spin.(.pic)
    + added dimentions of how the quotes will display. (.quote)
    
    - edited the index.html -
    + added created two seprate quotes which fade out of eacher other using
    a java script. (linked to the quoteloop class)
    + added images to show the people who gave me quotes. (class .pic)
    
    - edited the index.js -
    + added a fade feature that will change between outlined quotes after a
    spesific time has passed.
    
    - edited the Images folder -
    + added dec.jpg
    + added joe.jpg

--- PortfolioWebsite/js/index.js
+++ PortfolioWebsite/js/index.js
@@ -31,3 +31,16 @@ $(window).scroll(function() {
         $('.main_nav').removeClass('sticky');
     }
 });
+
+
+/*----------------------------------------------------*/
+/* Quote Loop
+------------------------------------------------------ */
+
+function fade($ele) {
+    $ele.fadeIn(1000).delay(3000).fadeOut(1000, function() {
+        var $next = $(this).next('.quote');
+        fade($next.length > 0 ? $next : $(this).parent().children().first());
+   });
+}
+fade($('.quoteLoop > .quote').first());
