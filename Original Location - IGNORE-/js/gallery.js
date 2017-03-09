ee4c8f1a2dbd7a9884073f9a3bb28c164937231e
P14182505 <P14182505@my365.dmu.ac.uk>
2017-03-09 12:27:01 +0000

    Commit 18 [09/3/17]
    - edited folder format -
    + changed the loaction of files to inkeep with assingment guidelines.
    + renamed the Portfolio website  - Original Website - IGNORE -, the file
    location should be disregarded.

---
+++ Original Location - IGNORE-/js/gallery.js
@@ -0,0 +1,15 @@
+/*----------------------------------------------------*/
+/* Gallery Zoom & Visable
+------------------------------------------------------ */
+$(document).ready(function(){
+  $("[class^='thumbnail-']").click(function(){
+    $("[class^='thumbnail-']").slideToggle("fast");
+    $(this).next("[class^='large-']").slideToggle();
+  });
+  
+  $(".close").click(function(){
+    $("[class^='large-']:visible").toggle();
+    $("[class^='thumbnail-']").fadeToggle("fast");; 
+  }); 
+  
+});
